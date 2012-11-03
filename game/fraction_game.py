f3f54f0d0731bab9eb69a04ccc053ab20edbcdfc
Stephen Herbein <stephen272@gmail.com>
2012-11-03 18:08:46 -0400

    Added comments for what I have done so far

--- game/fraction_game.py
+++ game/fraction_game.py
@@ -3,6 +3,14 @@ import spyral
 HEIGHT = 1200
 WIDTH = 900
 
+# The Fraction Mini-Game
+# The farmer pulls a lever to dispense water into a bucket
+# You need to solve a word problem to figure out how much
+# water you need in the bucket
+# If you run into problems, it sends you to the Fraction Tools
+# scene, where you can visulaize the fractional problem using
+# the widgets in the scene
+
 class FractionGame(spyral.Scene):
     def __init__(self, *args, **kwargs):
 
